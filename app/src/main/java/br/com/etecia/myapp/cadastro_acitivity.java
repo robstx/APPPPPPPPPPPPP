fc578cc9a55c693f5a7927ab2645bfa3870b1f57
RafaelGMC <rafael.g.de.m.carvalho@hotmail.com>
2023-09-28 22:04:59 -0300

    dasdsa5dsadsaertft

---
+++ app/src/main/java/br/com/etecia/myapp/cadastro_acitivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cadastro_acitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_layout);
+    }
+}
